1dbdd7804f3d6f2284ec1c8ea78c4d0821cd6c23
bijanmmarkes <bijan@stagerap.com>
2020-04-15 18:40:34 -0700

    initial dockerifle with python, conda, repo cloning, and install/startup scripts.

---
+++ Docker/scripts/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source scripts/settings.sh
+
+# v4l2loopback
+sudo apt install v4l2loopback-dkms
+
+source $(conda info --base)/etc/profile.d/conda.sh
+conda create -y -n $CONDA_ENV_NAME python=3.8
+conda activate $CONDA_ENV_NAME
+
+pip install face-alignment pyfakewebcam
+
+# FOMM
+git clone https://github.com/alievk/first-order-model.git fomm
+pip install -r fomm/requirements.txt
+pip install requests
